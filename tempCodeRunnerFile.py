0b5e5d0bf51b8a5f9914c7bae7e5695091c6d232
Sukriti Sharma <sukriti.sharma.met21@itbhu.ac.in>
2025-07-23 00:11:58 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+# Load a COCO-pretrained YOLO11n model
+model = YOLO("yolo11n.pt")
+
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model("YOLO/bus.jpg")
